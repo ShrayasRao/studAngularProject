932a6283d0c9bd6e4527711a8e38ce5332ec1252
ShreyasRao <raos1031@gmail.com>
2020-11-29 11:20:00 +0530

    version 1.0 commit

---
+++ src/app/student/stab.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "stab",
+    templateUrl: './stab.component.html',
+    styleUrls: ['./stab.component.css']
+})
+export class SComponent{
+
+    
+sarr=[{id:"1", name:"aaa", javamarks:"90",mathsmarks:"80",sciencemarks:"95" },
+    {id:"2", name:"bbb", javamarks:"55",mathsmarks:"60",sciencemarks:"85" },
+    {id:"3", name:"ccc", javamarks:"70",mathsmarks:"85",sciencemarks:"75" }];
+    
+}
